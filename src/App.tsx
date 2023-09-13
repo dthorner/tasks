1ae0edac4ba1cf3eca34ef2bf253e09acf52c7ce
dthorner <dthorner@udel.edu>
2023-09-13 01:53:28 -0400

    Added hello world to App

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,7 @@ function App(): JSX.Element {
             </header>
             <p>
                 Edit <code>src/App.tsx</code> and save. This page will
-                automatically reload. Devlan Horner
+                automatically reload. Devlan Horner Hello World
             </p>
         </div>
     );
