02e652f99d03cf25c5342c5306d4119af59ef3cd
dthorner <dthorner@udel.edu>
2023-09-17 00:15:15 -0400

    tripleNumbers completed

--- src/arrays.ts
+++ src/arrays.ts
@@ -17,7 +17,10 @@ export function bookEndList(numbers: number[]): number[] {
  * number has been tripled (multiplied by 3).
  */
 export function tripleNumbers(numbers: number[]): number[] {
-    return numbers;
+    const nums = numbers.map((current: number): number => {
+        return current * 3;
+    });
+    return nums;
 }
 
 /**
